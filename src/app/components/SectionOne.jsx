fcc193b490448257af8004400bcef3db766fcbcd
Bsoluwanjoba <bsoluwanjoba@gmail.com>
2024-11-06 10:06:26 +0100

    feat

--- src/app/components/SectionOne.jsx
+++ src/app/components/SectionOne.jsx
@@ -18,7 +18,7 @@ export default function SectionOne() {
 
             <div className='flex gap-2 md:mt-0 mt-[19em]'>
                 <Image src='/Mask group.png' height={460} width={340} alt="images" className="md:h-[460px] h-[320px] md:w-[340px] w-[200px]"/>
-                    <div className="space-y-4 md:mt-0 mt-4">
+                    <div className="space-y-4 ">
                     <Image src='/shot-two-young-black-females-looking-phone-together-feeling-excited-surprised.png' height={220} width={340} alt="images" className="rounded-2xl md:h-[220px] h-[150px] md:w-[340px] w-[159px]" />
                     <Image src='/cheerful-businessman-taking-pictures-mobile-phone-posing-with-coffee-cup-camera-happy-freelancer-using-smartphone-take-photos-coffee-break-enjoying-modern-technology.png' height={260} width={440} alt="images" className="rounded-2xl md:h-[220px] h-[150px] md:w-[340px] w-[159px]" />
                    
